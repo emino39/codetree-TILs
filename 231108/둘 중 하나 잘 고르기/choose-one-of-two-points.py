3c2fdea39dc081262ab186b4424889e552bb5448
emino39 <emino39@gmail.com>
2023-11-08 18:00:04 +0900

    Title: 둘 중 하나 잘 고르기, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 231108/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 231108/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -6,18 +6,18 @@ for n in range(2*N):
     red.append(r)
     blue.append(b)
 
-dp_b = [[-1] * (2 * N + 1) for _ in range(2 * N + 1)]
-dp_b[0][0] = 0
-dp_r = [[-1] * (2 * N + 1) for _ in range(2 * N + 1)]
-dp_r[0][0] = 0
+
+dp = [[-1] * (2 * N + 1) for _ in range(2 * N + 1)]
+dp[0][0] = 0
 
 # 빨강을 선택하지 않는 경우는 파랑을 선택하는 경우로 간주
 for i in range(1, 2 * N + 1):
-    dp_b[i][0] = dp_b[i-1][0]
-    dp_r[i][0] = dp_r[i-1][0]
+    dp[i][0] = dp[i-1][0]
 
-    for j in range(1, i+1):
-        dp_b[i][j] = max(dp_b[i-1][j] + red[i], dp_b[i-1][j-1] + blue[i])
-        dp_r[i][j] = max(dp_r[i-1][j] + blue[i], dp_r[i-1][j-1] + red[i])
+    for j in range(i+1):
+        if j > 0:
+            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + red[i])
+        if i - j > 0:
+            dp[i][j] = max(dp[i][j], dp[i-1][j] + blue[i])
 
-print(max(dp_b[2*N][N], dp_r[2*N][N]))
+print(dp[2*N][N])
