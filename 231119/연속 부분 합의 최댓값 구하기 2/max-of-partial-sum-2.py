e3d162cbf79e8e166975dabe87f4fd5b1e9e7c25
emino39 <emino39@gmail.com>
2023-11-19 17:52:17 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 131ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 231119/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = list(map(int, input().split()))
+total = arr[0]
+# max_total = (-1000) * n
+max_total = total
+
+for i in range(1, n):
+    if total + arr[i] < arr[i]:
+        total = arr[i]
+        max_total = max(max_total, total)
+
+    else:
+        if total + arr[i] < total:
+            max_total = max(max_total, total)
+            total = arr[i]
+        else:
+            total += arr[i]
+            max_total = max(max_total, total)
+
+print(max_total)
