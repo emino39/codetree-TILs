6e9afd797ad92efc65c342be0f4f19c936952c5b
emino39 <emino39@gmail.com>
2023-11-19 17:02:29 +0900

    Title: 동전 더하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231119/동전 더하기/add-coins.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+cnt = 0
+
+for i in range(n-1, -1, -1):
+    if k == 0:
+        break
+    
+    if k < coins[i]:
+        continue
+    
+    cnt += (k // coins[i])
+    k = k % coins[i]
+
+print(cnt)
