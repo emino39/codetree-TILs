28bc744b901f19633a86ede2c4c611ced169a0c3
emino39 <emino39@gmail.com>
2023-11-20 11:40:30 +0900

    Title: 숫자 합치기, Time: 365ms, Memory: 31MB, Status: Passed - CodeTree

--- 231120/숫자 합치기/merge-numbers.py
+++ 231120/숫자 합치기/merge-numbers.py
@@ -1,17 +1,18 @@
+from heapq import heappush, heappop, heapify
+
 n = int(input())
 arr = list(map(int, input().split()))
+heapify(arr)
 cost = 0
 
 while True:
     if len(arr) < 2:
         break
-    
-    arr = sorted(arr)
 
-    n1 = arr.pop(0)
-    n2 = arr.pop(0)
+    n1 = heappop(arr)
+    n2 = heappop(arr)
 
     cost += (n1 + n2)
-    arr.append(n1 + n2)
+    heappush(arr, n1+n2)
 
 print(cost)
